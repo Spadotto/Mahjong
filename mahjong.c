2778c3f50514e021358c94c0458a7fe3bcc1c90b
Ellen <52865288+Spadotto@users.noreply.github.com>
2020-08-03 16:38:08 -0300

    Add timer

--- mahjong.c
+++ mahjong.c
@@ -1,27 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include<windows.h>
 
 int main(){
 	char tabuleiro[10][10];
 	char pecas[100] = {'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'G', 'G', 'G', 'G', 'G', 'G','H', 'H', 'H', 'H', 'H', 'H', 'I', 'I', 'I', 'I', 'I', 'I', 'J', 'J', 'J', 'J', 'J', 'J', 'K', 'K', 'K', 'K', 'L', 'L', 'L', 'L', 'M', 'M', 'M', 'M', 'N', 'N', 'N', 'N', 'O', 'O', 'P', 'P', 'Q', 'Q', 'R', 'R'};
-	int i, j, aux, vencedor = 0; 
+	int i, j, aux, vencedor = 0;
 	int pecasrest;
 	int borda[10][10] = {0};
 	int l1, c1, sum1;
-	int l2, c2, sum2;	
+	int l2, c2, sum2;
+	int nivel, valido = 0;
+	time_t hora1, hora2;
+	int temporest = 600, hora3 = NULL;
+
 	srand(time(NULL));
-	
-	system("cls");
-	
+
+	printf("\nAtecao! Ao iniciar a partida voce tera 10 min!\n Boa sorte!!\n");
+
 	for(i=0; i<10; i++){//coloca '1' nas bordas da matriz de '0'.
 		borda[0][i] = 1;
 		borda[10-1][i] = 1;
 		borda[i][0] = 1;
 		borda[i][10-1] = 1;
 	}
-	
+
 	for(i=0; i<10; i++){//preenche com as peças.
 		for(j=0; j<10; j++){
 			aux = rand()%100;
@@ -34,7 +37,9 @@ int main(){
 			pecas[aux] = 'X';
 		}
 	}
-	
+
+	hora1 = time(NULL);
+
 	printf("\n     0       1    2    3    4    5    6    7    8    9    10\n");
 	printf("\n	+-------------------------------------------------------+\n");
 	for(i=0; i<10; i++){
@@ -52,30 +57,37 @@ int main(){
 		printf("	|\n	|                                                       |\n");
 	}
 	printf("	+-------------------------------------------------------+\n");
-	
-	while(pecasrest != 100){
+
+	while(temporest > hora3){
+		hora2 = time(NULL);
+
+		hora3 = hora2 - hora1;//diferença da hora de inicio de exucução com a atual.
+
+		printf("\n%d\n", hora3);
 		scanf("%d %d", &l1, &c1);
 		scanf("%d %d", &l2, &c2);
 		l1--;
 		c1--;
 		l2--;
 		c2--;
-		sum1 = l1*l1-c1;
-		sum2 = l2*l2-c2;
+		sum1 = l1*l1-c1*c1;
+		sum2 = l2*l2-c2*c2;
+
 		if(tabuleiro[l1][c1] == tabuleiro[l2][c2] && borda[l1][c1] != 0 && borda[l2][c2] != 0 && sum1 != sum2 && tabuleiro[l1][c1] != ' '){
 			tabuleiro[l1][c1] = ' ';
 			tabuleiro[l2][c2] = ' ';
-		
+
 			//transforma os valores ao redor em borda.
 			borda[l1-1][c1] = 1;
 			borda[l1][c1-1] = 1;
 			borda[l1+1][c1] = 1;
 			borda[l1][c1+1] = 1;
+
 			borda[l2-1][c2] = 1;
 			borda[l2][c2-1] = 1;
 			borda[l2+1][c2] = 1;
 			borda[l2][c2+1] = 1;
-			
+
 			system("cls");
 			printf("\n     0       1    2    3    4    5    6    7    8    9    10\n");
 			printf("\n	+-------------------------------------------------------+\n");
@@ -97,25 +109,29 @@ int main(){
 		}else{
 			printf("	\nJogada invalida!\nTente novamente!");
 		}
-		
+		vencedor = 0;
+
 		pecasrest=0;
 		for(i=0; i<10; i++){//contador para saber se já acabaram todas as peças do tabuleiro.
 			for(j=0; j<10; j++){
 				if(tabuleiro[i][j] == ' '){
 					pecasrest++;
-					vencedor = 1;
+					vencedor += 1;
 				}
 			}
 		}
+		if(pecasrest == 100){
+			break;
+		}
 	}
 	system("cls");
-	
-	if(vencedor){
+
+	if(!vencedor == 100){
 		printf("\n	  PARABENS TU VENCESTES!!!\n\n");
 	}else{
 		printf("\n	  VOCE PERDEU!\n\tACABOU O TEMPO!\n\n");
 	}
-	
+
 	system("PAUSE");
 	return 0;
 }
